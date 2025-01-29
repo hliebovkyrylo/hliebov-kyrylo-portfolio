aeb6d295a72c9af8f8c86a67dbc576e29f74943a
hliebovkyrylo <kirilglebov06@gmail.com>
2025-01-29 23:47:38 +0200

    feat(web): implement user information update form with validation and API integration

--- src/lib/middleware/authMiddleware.ts
+++ src/lib/middleware/authMiddleware.ts
@@ -1,12 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import cookies from "js-cookie";
 import { verifyAccessToken } from "../utils/jwt";
 import { errorResponse } from "../utils/apiResponse";
 
 export const authMiddleware = (handler: Function) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    // const accessToken = cookies.get("accessToken");
-    const accessToken = req.headers.authorization;
+    const accessToken = req.cookies.accessToken;
 
     if (!accessToken) {
       return res.status(401).json(errorResponse("Unauthorized", 401));
--- src/pages/admin/update-info/index.tsx
+++ src/pages/admin/update-info/index.tsx
@@ -1,10 +1,21 @@
 import { UpdateInfoForm } from "@/components/admin";
 import { AdminLayout } from "@/components/Layout";
+import { api } from "@/lib/api/api";
+import { endpoints } from "@/lib/api/endpoints";
+import { useQuery } from "@tanstack/react-query";
 
 export default function UpdateInfo() {
+  const { data: user, isLoading: isLoadingUser } = useQuery({
+    queryKey: [endpoints.getUser()],
+    queryFn: () => api.getUser(),
+    select: (data) => data.data.data,
+  });
+
+  if (isLoadingUser) return <div>Loading...</div>;
+
   return (
     <AdminLayout>
-      <UpdateInfoForm />
+      <UpdateInfoForm user={user} />
     </AdminLayout>
   );
 }
