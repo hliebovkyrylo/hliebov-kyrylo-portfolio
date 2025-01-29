450d1e5caf0d0d878dc171e52d0d7c169c6e6d5a
hliebovkyrylo <kirilglebov06@gmail.com>
2025-01-29 23:51:34 +0200

    feat(web): fetch and display projects

--- src/pages/admin/projects/index.tsx
+++ src/pages/admin/projects/index.tsx
@@ -1,21 +1,35 @@
 import { Project } from "@/components/admin";
 import { AdminLayout } from "@/components/Layout";
+import { api } from "@/lib/api/api";
+import { endpoints } from "@/lib/api/endpoints";
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import { BiPlus } from "react-icons/bi";
 
 export default function Projects() {
+  const { data: projects, isLoading: isLoadingProjects } = useQuery({
+    queryKey: [endpoints.getAllProjects()],
+    queryFn: () => api.getAllProjects(),
+    select: (data) => data.data.data,
+  });
+
+  if (isLoadingProjects) return <div>Loading...</div>;
+
   return (
     <AdminLayout>
       <div className="grid grid-cols-4 gap-6">
-        {[...Array(13)].map((_, i) => (
+        {projects?.map((project, i) => (
           <Project
             key={i}
-            id={(i + 1).toString()}
-            name="Test project"
-            imageUrl="/photo.png"
+            id={project.id}
+            name={project.name}
+            imageUrl={project.imageUrl}
           />
         ))}
-        <Link href={"/admin/create-project"} className="bg-slate-800 hover:bg-slate-700 transition duration-500 ease-in-out rounded-xl flex justify-center items-center">
+        <Link
+          href={"/admin/create-project"}
+          className="bg-slate-800 hover:bg-slate-700 transition duration-500 ease-in-out rounded-xl flex justify-center items-center"
+        >
           <BiPlus size={64} />
         </Link>
       </div>
