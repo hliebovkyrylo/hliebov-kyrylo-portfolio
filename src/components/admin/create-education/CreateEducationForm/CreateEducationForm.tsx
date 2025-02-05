848e9121c8defabf694109698851614c10a2c3d0
hliebovkyrylo <kirilglebov06@gmail.com>
2025-02-05 13:11:35 +0200

    feat(web): implement create education form

--- src/components/admin/create-education/CreateEducationForm/CreateEducationForm.tsx
+++ src/components/admin/create-education/CreateEducationForm/CreateEducationForm.tsx
@@ -1,6 +1,41 @@
+import { api } from "@/lib/api/api";
+import { ErrorResponse } from "@/lib/utils/apiResponse";
+import {
+  CreateEducationInput,
+  createEducationSchema,
+} from "@/schemas/createEducationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export const CreateEducationForm = () => {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+
+  const { handleSubmit, register } = useForm<CreateEducationInput>({
+    resolver: zodResolver(createEducationSchema),
+  });
+
+  const mutation = useMutation({
+    mutationFn: api.createEducation,
+    onError: (response: AxiosError<ErrorResponse>) => {
+      setError(response.response?.data.error_message || null);
+    },
+    onSuccess: () => {
+      router.push("/admin/educations");
+    },
+  });
+
+  const onSubmit = handleSubmit((data: CreateEducationInput) => {
+    mutation.mutate(data);
+  });
+
   return (
-    <form className="flex flex-col max-w-96 w-full gap-3">
+    <form onSubmit={onSubmit} className="flex flex-col max-w-96 w-full gap-3">
+      <div>{error}</div>
       <div>
         <label className="font-thin text-xs block">NAME</label>
         <input
@@ -8,6 +43,7 @@ export const CreateEducationForm = () => {
           required
           className="px-3 py-2 rounded-lg bg-slate-600 w-full"
           placeholder="Enter name of education..."
+          {...register("name")}
         />
       </div>
       <div>
@@ -17,6 +53,7 @@ export const CreateEducationForm = () => {
           required
           className="px-3 py-2 rounded-lg bg-slate-600 w-full"
           placeholder="Enter years..."
+          {...register("years")}
         />
       </div>
       <div>
@@ -26,6 +63,7 @@ export const CreateEducationForm = () => {
           required
           className="px-3 py-2 rounded-lg bg-slate-600 w-full"
           placeholder="Enter specialization"
+          {...register("specialization")}
         />
       </div>
       <div>
@@ -34,6 +72,7 @@ export const CreateEducationForm = () => {
           required
           className="px-3 py-2 rounded-lg bg-slate-600 w-full"
           placeholder="Enter description..."
+          {...register("description")}
         />
       </div>
       <button
